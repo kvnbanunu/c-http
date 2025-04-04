87ea2b537f0183d2b0258a4e55c36b17e9c91e98
evin-gg <evin4369@gmail.com>
2025-04-03 21:32:30 -0700

    added post request handling and fixed overwriting array memory issues hopefully

--- include/handler.h
+++ include/handler.h
@@ -3,12 +3,28 @@
 
 #include <stdlib.h>
 
+#ifdef __APPLE__
+typedef size_t datum_size;
+#else
+typedef int datum_size;
+#endif
+
+#define TO_SIZE_T(x) ((size_t)(x))
+
+#define MAKE_CONST_DATUM(str) ((const_datum){(str), (datum_size)strlen(str) + 1})
+
 struct req_info
 {
     char *method;
     char *path;
     char *protocol;
 };
+
+typedef struct
+{
+    const void *dptr;
+    datum_size  dsize;
+} const_datum;
 
 void       *handle_request(void *arg);
 
--- src/handler.c
+++ src/handler.c
@@ -1,6 +1,9 @@
 #include "../include/handler.h"
 #include <arpa/inet.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
+#include <ndbm.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,7 +13,10 @@
 #include <time.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 1024
+#define FMT_BUFFER 50
+
+// #define BASE_TEN 10
 
 static void construct_response(int clientfd, const char *status, const char *body, const char *mime, size_t body_len)
 {
@@ -46,17 +52,23 @@ static void trim_protocol(char *protocol)
     }
 }
 
-static void parse_request(struct req_info *info, char *buffer)
+static void parse_request(struct req_info *info, const char *buffer)
 {
     char *save;
+    char  temp_buffer[BUFFER_SIZE];
 
-    info->method   = strtok_r(buffer, " ", &save);
+    strncpy(temp_buffer, buffer, BUFFER_SIZE);
+    info->method   = strtok_r(temp_buffer, " ", &save);
     info->path     = strtok_r(NULL, " ", &save);
     info->protocol = strtok_r(NULL, " ", &save);
     if(info->protocol)
     {
         trim_protocol(info->protocol);
     }
+
+    printf("PARSED METHOD: %s\n", info->method);
+    printf("PARSED PATH: %s\n", info->path);
+    printf("PARSED PROTOCOL: %s\n", info->protocol);
 }
 
 static const char *get_mime_type(const char *file_path)
@@ -129,31 +141,48 @@ static int file_verification(const char *file_path)
     int  retval;
     int  a;
     int  b;
-    char file_path_formatted[BUFFER_SIZE];
+    char file_path_formatted[FMT_BUFFER];
 
+    printf("VERIFYING PATH 1: %s\n", file_path);
     snprintf(file_path_formatted, sizeof(file_path_formatted), ".%s", file_path);
+    printf("VERIFYING PATH 2: %s\n", file_path);
+    printf("FORMMATED (SHOUDLd WIRKJ): %s\n", file_path_formatted);
 
+    file_path = file_path_formatted;
+    printf("PASSED IN FILE PATH: %s\n", file_path);
     a      = file_readable(file_path_formatted);
     b      = file_exists(file_path_formatted);
-    retval = -1;
+    retval = 0;
 
-    printf("%s\n", file_path_formatted);
     printf("readable: %d exists: %d\n", a, b);
 
-    if(file_readable(file_path_formatted) == 0 && file_exists(file_path_formatted) == 0)
+    // 404 case
+    if(b != 0)
     {
-        retval = 0;
+        printf("SHOULD BE 404\n");
+        retval = -1;
+        return retval;
     }
 
+    // 403 case
+    if(a != 0)
+    {
+        printf("IM BOUTTA 403\n");
+        retval = -2;
+        return retval;
+    }
+
+    printf("VERIFICATION SUCCEEDED\n");
     return retval;
 }
 
 static int open_requested_file(int *fd, const char *path)
 {
-    char file_path_formatted[BUFFER_SIZE];
+    char file_path_formatted[FMT_BUFFER];
 
     snprintf(file_path_formatted, sizeof(file_path_formatted), ".%s", path);
 
+    printf("OPENING DIR: %s\n", file_path_formatted);
     *fd = open(file_path_formatted, O_RDONLY | O_CLOEXEC);
     if(*fd < 0)
     {
@@ -187,6 +216,24 @@ static void construct_get_response404(int clientfd)
     construct_response(clientfd, "404 Not Found", body, "text/html", strlen(body));
 }
 
+static void construct_get_response405(int clientfd)
+{
+    const char body[] = "<html><body><h1>405 Unknown Method</h1></body></html>";
+    construct_response(clientfd, "405 Unknown Method", body, "text/html", strlen(body));
+}
+
+static void construct_get_response403(int clientfd)
+{
+    const char body[] = "<html><body><h1>403 Forbidden</h1></body></html>";
+    construct_response(clientfd, "403 Forbidden", body, "text/html", strlen(body));
+}
+
+static void construct_get_response500(int clientfd)
+{
+    const char body[] = "<html><body><h1>500 Internal Server Error</h1></body></html>";
+    construct_response(clientfd, "500 Internal Server Error", body, "text/html", strlen(body));
+}
+
 static void construct_get_response200(int clientfd, const char *mime, int filefd)
 {
     size_t  fileSize;
@@ -211,7 +258,95 @@ static void construct_get_response200(int clientfd, const char *mime, int filefd
     }
 }
 
-void *handle_request(void *arg)
+// static int get_content_length(char buffer[BUFFER_SIZE])
+// {
+//     const char *key = "Content-Length: ";
+//     int         keylen;
+//     char       *lenptr;
+//     char       *endptr;
+
+//     lenptr = strstr(buffer, key);
+//     keylen = (int)strlen(key);
+
+//     if(lenptr)
+//     {
+//         long length;
+
+//         lenptr += keylen;    // move past the key to the number part
+
+//         // Reset errno before the call
+//         errno = 0;
+
+//         length = strtol(lenptr, &endptr, BASE_TEN);    // base 10
+
+//         // Error checking
+//         if(errno != 0 || lenptr == endptr || length < 0 || length > INT_MAX)
+//         {
+//             return -1;
+//         }
+
+//         return (int)length;
+//     }
+
+//     return -1;
+// }
+
+static void get_body_pos(char buffer[BUFFER_SIZE], char **bodyptr)
+{
+    const char *key;
+
+    key = "\r\n\r\n";
+
+    *bodyptr = strstr(buffer, key);
+
+    if(*bodyptr)
+    {
+        *bodyptr += 4;
+    }
+}
+
+static int store_string(DBM *db, const char *key, const char *value)
+{
+    const_datum key_datum   = MAKE_CONST_DATUM(key);
+    const_datum value_datum = MAKE_CONST_DATUM(value);
+
+    return dbm_store(db, *(datum *)&key_datum, *(datum *)&value_datum, DBM_REPLACE);
+}
+
+static void store_in_db(const char *key, const char *value)
+{
+    // printf("%s, %s\n\n", key, value);
+    char db_name[] = "posts_db";    // cppcheck-suppress constVariable
+    DBM *db        = dbm_open(db_name, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+    if(!db)
+    {
+        perror("dbm_open");
+        return;
+    }
+
+    if(store_string(db, key, value) != 0)
+    {
+        perror("store_string");
+        dbm_close(db);
+        return;
+    }
+}
+
+static void tokenize_post(char *body)
+{
+    const char *key;
+    const char *value;
+    char       *save;
+
+    key   = strtok_r(body, "=", &save);
+    value = strtok_r(NULL, "=", &save);
+
+    // printf("%s, %s\n\n", key, value);
+    store_in_db(key, value);
+}
+
+__attribute__((noreturn)) void *handle_request(void *arg)
 {
     int             clientfd;
     int             requestedfd;
@@ -220,7 +355,8 @@ void *handle_request(void *arg)
     ssize_t         valread;
 
     clientfd = *(int *)arg;
-    valread  = read(clientfd, buffer, BUFFER_SIZE);
+    memset(buffer, 0, sizeof(buffer));
+    valread = read(clientfd, buffer, BUFFER_SIZE);
     if(valread < 0)
     {
         // handle reading error
@@ -228,29 +364,38 @@ void *handle_request(void *arg)
         pthread_exit(NULL);
     }
 
+    printf("INCOMING:\n%s\nEND BUFFER\n", buffer);
     // parse
     parse_request(&info, buffer);
-
     // check for get, head, post
     if(strcmp("GET", info.method) == 0)
     {
         // size_t      content_length;
         const char *mime;
+        int         verification;
 
-        printf("%s\n", info.path);
+        verification = file_verification(info.path);
+
+        printf("GET VERIOFICATION VALUE: %d\n", verification);
         // error handle if path is not real and stuff
-        if(file_verification(info.path) == -1)
+        if(verification == -1)
         {
             // send 404 error back to client
             construct_get_response404(clientfd);
-            return NULL;
+            pthread_exit(NULL);
         }
 
-        printf("opening requested file..\n");
+        else if(verification == -2)
+        {
+            // send 404 error back to client
+            construct_get_response403(clientfd);
+            pthread_exit(NULL);
+        }
+
         // handle
         if(open_requested_file(&requestedfd, info.path) == -1)
         {
-            fprintf(stderr, "Failed to open requested file");
+            construct_get_response500(clientfd);
             pthread_exit(NULL);
         }
 
@@ -262,27 +407,49 @@ void *handle_request(void *arg)
 
     else if(strcmp("HEAD", info.method) == 0)
     {
+        int verification;
+
+        verification = file_verification(info.path);
+
+        printf("HEAD VERIOFICATION VALUE: %d\n", verification);
         // error handle if path is not real and stuff
-        if(file_verification(info.path) == -1)
+        if(verification == -1)
         {
-            // send 404 back to client
-            construct_response(clientfd, "404", NULL, "text/html", 0);
-            exit(EXIT_FAILURE);
+            // send 404 error back to client
+            construct_get_response404(clientfd);
+            pthread_exit(NULL);
+        }
+
+        else if(verification == -2)
+        {
+            // send 404 error back to client
+            construct_get_response403(clientfd);
+            pthread_exit(NULL);
         }
 
         // handle head
         construct_response(clientfd, "200 OK", NULL, "text/html", 0);
     }
 
-    // else if(strcmp("POST", info.method) == 0)
-    // {
-    //     //handle post
-    // }
+    else if(strcmp("POST", info.method) == 0)
+    {
+        // handle post
+        // int   content_len;
+        char *bodyptr;
 
-    // else
-    // {
-    //     //handler error
-    // }
+        // content_len = get_content_length(buffer);
+        get_body_pos(buffer, &bodyptr);
+        tokenize_post(bodyptr);
+        construct_response(clientfd, "200 OK", NULL, "text/html", 0);
+        pthread_exit(NULL);
+    }
 
-    exit(EXIT_SUCCESS);
+    else
+    {
+        printf("NOT KNOWN\n");
+        // handler error
+        construct_get_response405(clientfd);
+    }
+
+    pthread_exit(NULL);
 }
